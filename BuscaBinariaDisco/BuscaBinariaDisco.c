50863fcb66effd422e8322b3a40f7ef6efecbb50
Luiz Felipi <lfjunionello@gmail.com>
2021-09-08 15:55:09 -0300

    Added Busca Binaria

---
+++ BuscaBinariaDisco/BuscaBinariaDisco.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct _Endereco Endereco;
+
+struct _Endereco{
+	char logradouro[72];
+	char bairro[72];
+	char cidade[72];
+	char uf[72];
+	char sigla[2];
+	char cep[8];
+	char lixo[2];
+}; 
+
+int buscaBinaria(FILE *f, char *target,long primeiro,long ultimo,int count){  // Retorna a quantidade de passos
+
+	Endereco e;
+	long meio;
+	rewind(f);
+
+	if(ultimo >= primeiro){
+
+		//int qt;	// Testa se foi possivel ler
+
+		count++;
+
+		meio = (primeiro+ultimo)/2;
+
+		printf("\nINICIO: %ld\tMEIO: %ld\tFINAL: %ld\n",primeiro,meio,ultimo);
+
+		fseek(f,(meio*sizeof(Endereco)),SEEK_SET);
+
+		fread(&e, sizeof(Endereco), 1, f);
+
+		if(strncmp(target,e.cep,8) == 0){
+			printf("========================\n");
+			printf("\nLogradouro: %.72s\nBairro: %.72s\nCidade: %.72s\nUF: %.72s\nSigla: %.2s\nCEP: %.8s\n\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
+			return count;
+		}
+		else{
+			if(strncmp(target,e.cep,8) > 0){
+				buscaBinaria(f,target,meio+1,ultimo,count);
+			}
+			
+			else if(strncmp(target,e.cep,8) < 0){
+				buscaBinaria(f,target,primeiro,meio-1,count);
+			}
+
+		}
+
+	}
+	else{
+		
+		printf("Não foi possivel encountrar o CEP\n");
+		return count;
+
+	}
+
+}
+
+
+
+int main(int argc, char**argv){
+
+	FILE *f;
+    Endereco e;
+    
+	int count = 0;
+	char target[8];
+	long tamanhoArquivo, primeiro, ultimo;
+
+	if(argc != 2){
+		fprintf(stderr, "USO: %s [CEP]", argv[0]);
+		return 1;
+	}
+
+	strncpy(target,argv[1],8);
+
+	f = fopen("cep_ordenado.dat","r");
+
+	fseek(f,0,SEEK_END);
+	tamanhoArquivo = ftell(f);
+
+	fseek(f,0,SEEK_SET);
+	printf("Tamanho do Arquivo: %ld\n", tamanhoArquivo);
+	printf("Tamanho do Registro: %ld\n", sizeof(Endereco));
+	printf("Tamanho do Arquivo em Registros: %ld\n", tamanhoArquivo/sizeof(Endereco));
+
+
+	primeiro = 0;
+	ultimo = (tamanhoArquivo/sizeof(Endereco))-1;
+
+	count = buscaBinaria(f, target, primeiro, ultimo, count);
+
+	printf("========================\n");
+	printf("PASSOS TOTAIS: %i",count);
+
+    	
+	fclose(f);
+	return 0;
+}
